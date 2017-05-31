4d8f1ec39086f10c9abefb04756f699c62343715
Capitao J <jonathan.capitao.12587@gmail.com>
2017-05-31 13:09:20 +0200

    transformation test existant pour mock

--- src/test/java/ch/hesge/capitao/mv421/TurnNGTest.java
+++ src/test/java/ch/hesge/capitao/mv421/TurnNGTest.java
@@ -1,5 +1,7 @@
 package ch.hesge.capitao.mv421;
 
+import java.util.ArrayList;
+import org.mockito.Mockito;
 import static org.testng.Assert.*;
 
 /**
@@ -15,13 +17,37 @@ public class TurnNGTest {
     @org.testng.annotations.Test
     public void testIsGameWon() {
         System.out.println("isGameWon");
-        int[] test = {4,2,1};
-        int[] test2 = {2,4,1};
-        int[] test3 = {1,2,4};
-        int[] test4 = {1,4,2};
-        int[] test5 = {3,3,3};
-        int[] test6 = {9,7,5};
-        int[] test7 = {4,4,4};
+          
+        Dice d1 = Mockito.mock(Dice.class);
+        Dice d2 = Mockito.mock(Dice.class);
+        Dice d3 = Mockito.mock(Dice.class);
+        Dice d4 = Mockito.mock(Dice.class);
+        Dice d5 = Mockito.mock(Dice.class);
+        Dice d6 = Mockito.mock(Dice.class);
+        
+        Mockito.when(d1.getValue()).thenReturn(1);
+        Mockito.when(d2.getValue()).thenReturn(2);
+        Mockito.when(d3.getValue()).thenReturn(3);
+        Mockito.when(d4.getValue()).thenReturn(4);
+        Mockito.when(d5.getValue()).thenReturn(5);
+        Mockito.when(d6.getValue()).thenReturn(6);
+        
+        ArrayList<Dice> test = new ArrayList<Dice>(); 
+        ArrayList<Dice> test2 = new ArrayList<Dice>(); 
+        ArrayList<Dice> test3 = new ArrayList<Dice>(); 
+        ArrayList<Dice> test4 = new ArrayList<Dice>(); 
+        ArrayList<Dice> test5 = new ArrayList<Dice>(); 
+        ArrayList<Dice> test6 = new ArrayList<Dice>(); 
+        ArrayList<Dice> test7 = new ArrayList<Dice>(); 
+        
+        test.add(d4);test.add(d2);test.add(d1);
+        test2.add(d2);test2.add(d4);test2.add(d1);
+        test3.add(d1);test3.add(d2);test3.add(d4);
+        test4.add(d1);test4.add(d4);test4.add(d2);
+        test5.add(d3);test5.add(d3);test5.add(d3);
+        test6.add(d6);test6.add(d1);test6.add(d5);
+        test7.add(d4);test7.add(d4);test7.add(d4);
+        
         assertTrue(new Turn(test).isGameWon());
         assertTrue(new Turn(test2).isGameWon());
         assertTrue(new Turn(test3).isGameWon());
